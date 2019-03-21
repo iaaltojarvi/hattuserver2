445f145ccb657ccd9c493a2b47b6333c725d8d8a
rinskinen <riina.j.moilanen@gmail.com>
2019-03-21 15:10:38 +0200

    Testing encoding more

--- dbservice.js
+++ dbservice.js
@@ -18,7 +18,7 @@ const conopts = {
   connectionString: process.env.DATABASE_URL,
   ssl: true,
   //sslmode: "require"
-  client_encoding: "utf-8"
+  client_encoding: "cp850"
   // sslmode: "require"
 };
 
